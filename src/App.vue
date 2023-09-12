82a44ad87fcbc06e047f7952330e39c8ec30d3b9
EMANUEL DA SILVA NASCENTE <106539519+enascentedev@users.noreply.github.com>
2023-09-12 15:06:25 -0300

    aula 4

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,25 @@
 			<img src="https://pinia.vuejs.org/logo.svg" alt="pinia logo" />
 			<h1>Aprendendo Pinia</h1>
 		</header>
+		<!-- task list -->
+		<div class="task-list">
+			<div v-for="task in taskStore.tasks" :key="task.id">
+				<!-- <p>{{ task.title }}</p> -->
+				<TaskDetails :task="task" />
+			</div>
+		</div>
 	</main>
 </template>
 
 <script>
-export default {};
+import TaskDetails from "./components/TaskDetails.vue";
+import { useTaskStore } from "./stores/TaskStore";
+export default {
+	components: { TaskDetails },
+	setup() {
+		const taskStore = useTaskStore();
+
+		return { taskStore };
+	},
+};
 </script>
